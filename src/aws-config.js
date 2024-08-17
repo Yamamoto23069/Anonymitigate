25af1af74b282ff314cfb2478e011809a53cf980
Lê Thái Sơn <161118860+LyMuc@users.noreply.github.com>
2024-08-17 21:58:15 +0700

    Update aws-config.js

--- src/aws-config.js
+++ src/aws-config.js
@@ -4,5 +4,5 @@ import AWS from 'aws-sdk';
 // Cấu hình AWS SDK
 AWS.config.update({
   region: "ap-southeast-1", // Thay đổi theo khu vực của bạn
-  credentials: new AWS.Credentials('AKIA6ODU2Q6SBFV3XTVZ', 'rwGkQvwqk0hKLdcChiSRskyrpqvSQ8obbzmc2QdW')
+  credentials: new AWS.Credentials('', '')
 });
